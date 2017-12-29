234975b48524865585dbee916227666440d285ec
Jordi Freixa <jordi.freixa11@gmail.com>
2017-12-29 15:56:05 +0100

    Add styled components

--- src/components/day/Day.js
+++ src/components/day/Day.js
@@ -1,9 +1,18 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    display: inline-block;
+    color: ${props => props.actualMonth ? 'black' : 'gray'};
+    text-align: center;
+    width: 1.5em;
+`;
 
 export default class Day extends PureComponent {
     render() {
-        return <Fragment>{this.props.number}</Fragment>;
+        const { actualMonth, number } = this.props
+        return <Wrapper actualMonth={actualMonth}>{number}</Wrapper>;
     }
 }
 
